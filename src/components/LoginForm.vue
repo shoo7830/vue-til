793cf3be01e5b39cc004bed9ffd0a705117e7eba
Aria Kim <shoo7830@gmail.com>
2020-02-08 22:51:49 +0900

    환경 설정, 로그인 페이지 구성

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">password: </label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      pasword: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      axios.post();
+    },
+  },
+};
+</script>
+
+<style></style>
