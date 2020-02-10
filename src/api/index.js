91eb316c44f54474a4500a2c598cd844d6c4c49c
Aria Kim <shoo7830@gmail.com>
2020-02-10 21:41:44 +0900

    token api 처리

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    Authorization: 'test1234',
+  },
 });
 
 function registerUser(userData) {
