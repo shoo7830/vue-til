07ffa652149a01e6912801d9f36423fe754436bb
Aria Kim <shoo7830@gmail.com>
2020-02-13 23:03:59 +0900

    컴포넌트 분리, props, loading

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,29 +2,39 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <ul>
-        <li v-for="postItem in postItems" :key="postItem._id">
-          {{ postItem.title }}
-        </li>
+      <div v-if="isLoading">
+        Loading...
+      </div>
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        />
       </ul>
     </div>
   </div>
 </template>
-
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
 import { fetchPosts } from '@/api/index';
 
 export default {
+  components: {
+    PostListItem,
+  },
   data() {
     return {
       postItems: [],
+      isLoading: false,
     };
   },
   methods: {
     async fetchData() {
+      this.isLoading = true;
       const { data } = await fetchPosts();
+      this.isLoading = false;
       this.postItems = data.posts;
-      console.log(data.posts);
     },
   },
   created() {
