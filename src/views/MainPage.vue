08e00469f260fce861f359d12b43d7c8558cc5cd
Aria Kim <shoo7830@gmail.com>
2020-02-15 21:44:00 +0900

    스피너 추가

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,9 +2,7 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <div v-if="isLoading">
-        Loading...
-      </div>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
       <ul v-else>
         <PostListItem
           v-for="postItem in postItems"
@@ -17,11 +15,13 @@
 </template>
 <script>
 import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import { fetchPosts } from '@/api/index';
 
 export default {
   components: {
     PostListItem,
+    LoadingSpinner,
   },
   data() {
     return {
